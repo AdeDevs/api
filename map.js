c3390e17c487232a6eb84ace9f6c22392a536d1f
AdeDevs <adeyemiakinyemi01@gmail.com>
2025-05-21 20:01:14 +0100

    updt

---
+++ map.js
@@ -0,0 +1,12 @@
+const mymap = L.map('issMap').setView([0, 0], 1);
+const attribution = '&copy <a href="https://www.openstreetmap.org/copyright"> OpenStreet </a> contributors';
+const tileURL = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png'
+const tiles = L.tileLayer(tileURL, {attribution} ).addTo(mymap)
+var myIcon = L.icon({
+    iconUrl: 'iss.png',
+    iconSize: [18, 45],
+    iconAnchor: [22, 94],
+    popupAnchor: [-3, -76],
+});
+
+const marker = L.marker([0, 0], {icon: myIcon}).addTo(mymap)
--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 const url = 'https://api.wheretheiss.at/v1/satellites/25544'
+
 async function GetISS() {
     const res = await fetch(url)
     const data = await res.json();
+    const {latitude, longitude} = data;
     document.getElementById('lat').innerText = data.latitude
     document.getElementById('lon').innerText = data.longitude
+
+    marker.setLatLng([longitude, latitude]).addTo(mymap)
 }
-
-// GetISS().catch(e => {
-//     console.log(e)
-// })
-
-setInterval( () => GetISS(), 1000)
+setInterval( () => GetISS(), 2000)
